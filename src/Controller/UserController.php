8d29614bb5e81c8d99e1a3a43fef8387fd4f706c
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2021-01-12 19:57:32 +0000

    Update user CRUD

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Commande;
 use App\Entity\Produit;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -15,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProduitRepository;
-use DoctrineExtensions\Query\Mysql\Month;
+
 /**
  * @Route("/user")
  */
@@ -24,56 +23,61 @@ class UserController extends AbstractController
     /**
      * @Route("/", name="user_index", methods={"GET"})
      */
-    public function index(UserRepository $userRepository,Request $request): Response
+    public function index(UserRepository $userRepository, Request $request): Response
     {
+        $session = $request->getSession();
+        $name = $session->get('name');
+
         return $this->render('user/index.html.twig', [
+            'name' => $name,
             'users' => $userRepository->findAll(),
         ]);
     }
     /**
      * @Route("/dash", name="dashboard")
      */
-    public function dash(Request $request,CommandeRepository $order,ProduitRepository $product)
+    public function dash(Request $request, CommandeRepository $order, ProduitRepository $product)
     {
         $session = $request->getSession();
         $name = $session->get('name');
-        $numberOrder=count($order->findAll());
-        $stocknumber=0;
-        $allProducts=$product->findAll();
-        for ($i=0; $i <count($allProducts) ; $i++) { 
-             $stocknumber=$stocknumber+$allProducts[$i]->getStock();    
+        $numberOrder = count($order->findAll());
+        $stocknumber = 0;
+        $allProducts = $product->findAll();
+        for ($i = 0; $i < count($allProducts); $i++) {
+            $stocknumber = $stocknumber + $allProducts[$i]->getStock();
         }
         return $this->render('user/dashboard.html.twig', [
             'name' => $name,
-            'numberOrder'=>$numberOrder,
-            'stockNumber'=>$stocknumber,
+            'numberOrder' => $numberOrder,
+            'stockNumber' => $stocknumber,
         ]);
     }
     /**
      * @Route("/dash/show", name="dashboard_show")
      */
-    public function dashShow(Request $request,CommandeRepository $orderRepository):Response
+    public function dashShow(Request $request, CommandeRepository $orderRepository): Response
     {
-    
-    $productStats=array();
-    $orderStats=array();
+
+        $productStats = array();
+        $orderStats = array();
         $em = $this->getDoctrine()->getRepository(Produit::class);
-        $allProducts=$em->findAll();
-        for ($i=0; $i <count($allProducts) ; $i++) {
-            $productStats[$allProducts[$i]->getLibelle()]= count($em->findByLibelle($allProducts[$i]->getLibelle()));
+        $allProducts = $em->findAll();
+        for ($i = 0; $i < count($allProducts); $i++) {
+            $productStats[$allProducts[$i]->getLibelle()] = count($em->findByLibelle($allProducts[$i]->getLibelle()));
         }
-        
-        
-        for ($i=1; $i <13 ; $i++) {
-            $orderMonth=count($orderRepository->findByDate((int) date('Y'),(int) $i));
-        
-       
-            $orderStats[(int) $i]=$orderMonth;                  
+
+
+        for ($i = 1; $i < 13; $i++) {
+            $orderMonth = count($orderRepository->findByDate((int) date('Y'), (int) $i));
+
+
+            $orderStats[(int) $i] = $orderMonth;
         }
         arsort($productStats);
-        $productStats=array_slice($productStats,0,5);
-        return $this->json(["labelsOrder"=>["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"],"dataOrder"=>array_values($orderStats),"labelsProducts"=>array_keys($productStats),"dataProducts"=>array_values($productStats)],200);
+        $productStats = array_slice($productStats, 0, 5);
+        return $this->json(["labelsOrder" => ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"], "dataOrder" => array_values($orderStats), "labelsProducts" => array_keys($productStats), "dataProducts" => array_values($productStats)], 200);
     }
+
     /**
      * @Route("/new", name="user_new", methods={"GET","POST"})
      */
@@ -86,6 +90,7 @@ class UserController extends AbstractController
         $name = $session->get('name');
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
+            $user->setPwd(password_hash($user->getPwd(), PASSWORD_DEFAULT));
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -93,74 +98,82 @@ class UserController extends AbstractController
         }
 
         return $this->render('user/new.html.twig', [
-            'user' => $user,'name'=>$name,
+            'user' => $user, 'name' => $name,
             'form' => $form->createView(),
         ]);
     }
-/**
-    * @Route("/login", name="user_login", methods={"GET","POST"})
-    */
-   public function login(Request $request,UserRepository $userRepository,CommandeRepository $order,ProduitRepository $product): Response
-   {
-       $session = $request->getSession();
-       $session->clear();
-       $user = new User();
-       $form = $this->createFormBuilder($user)
-       ->add('login', TextType::class,[
-        'attr' => [
-            'placeholder' => 'Taper votre login',
-                    ],
-       
-    ])
-       ->add('pwd', PasswordType::class,[
-        'attr' => [
-            'placeholder' => 'Taper votre Password',
-                    ],
-       
-    ])
 
-        ->getForm();
+    /**
+     * @Route("/login", name="user_login", methods={"GET","POST"})
+     */
+    public function login(Request $request, UserRepository $userRepository, CommandeRepository $order, ProduitRepository $product): Response
+    {
+        $session = $request->getSession();
+        $session->clear();
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('login', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Taper votre login',
+                ],
 
-       $form->handleRequest($request);
+            ])
+            ->add('pwd', PasswordType::class, [
+                'attr' => [
+                    'placeholder' => 'Taper votre Password',
+                ],
 
-       if ($form->isSubmitted()) {
-           $pwd   = $user->getPwd();
-           $login = $user->getLogin();
-           $user1 = $userRepository->findOneBy(array('login'=>$login,
-           'pwd'=>$pwd));
-          if (!$user1)
-          {
-           $this->get('session')->getFlashBag()->add('info',
-            'Login Incorrecte Vérifier Votre Login  ....');
-          }
-          else
-          {
-           if (!$session->has('name'))
-           {
-               $session->set('name',$user1->getUserName());
-               $name = $session->get('name');
-               $numberOrder=count($order->findAll());
-               $stocknumber=0;
-               $allProducts=$product->findAll();
-               for ($i=0; $i <count($allProducts) ; $i++) { 
-                    $stocknumber=$stocknumber+$allProducts[$i]->getStock();    
-               }
-             
-                   return $this->render('user/dashboard.html.twig', [
-                     'name'=>$name,
-                     'numberOrder'=>$numberOrder,
-                     'stockNumber'=>$stocknumber,
-         
-                   ]); 
-               }
-      }
+            ])
+
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $pwd   = $user->getPwd();
+            $login = $user->getLogin();
+            $user1 = $userRepository->findOneBy(array('login' => $login));
+            if (!$user1 || !password_verify($pwd, $user1->getPwd())) {
+                $this->get('session')->getFlashBag()->add(
+                    'info',
+                    'Login Incorrecte Vérifier Votre Login  ....'
+                );
+            } else {
+                if (!$session->has('name')) {
+                    $session->set('name', $user1->getUserName());
+                    $name = $session->get('name');
+                    $numberOrder = count($order->findAll());
+                    $stocknumber = 0;
+                    $allProducts = $product->findAll();
+                    for ($i = 0; $i < count($allProducts); $i++) {
+                        $stocknumber = $stocknumber + $allProducts[$i]->getStock();
+                    }
+
+                    return $this->render('user/dashboard.html.twig', [
+                        'name' => $name,
+                        'numberOrder' => $numberOrder,
+                        'stockNumber' => $stocknumber,
+
+                    ]);
+                }
+            }
+        }
+
+        return $this->render('user/login.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
     }
+    /**
+     * @Route("/logout", name="user_logout", methods={"GET"})
+     */
+    public function logout(Request $request): Response
+    {
+        $session = $request->getSession();
+        $session->clear();
 
-   return $this->render('user/login.html.twig', [
-    'user' => $user,
-    'form' => $form->createView(),
-]);
-   }
+        return $this->redirectToRoute('user_login');
+    }
 
 
     /**
@@ -182,6 +195,7 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('user_index');
@@ -198,7 +212,7 @@ class UserController extends AbstractController
      */
     public function delete(Request $request, User $user): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($user);
             $entityManager->flush();
